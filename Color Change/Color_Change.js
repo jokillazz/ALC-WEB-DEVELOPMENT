7d63cf3988ad5b765d19b0a73109136b7881f37a
Unknown <joseph.c070@stu.nebo.edu>
2018-03-20 10:29:36 -0600

    color.js

---
+++ Color Change/Color_Change.js
@@ -0,0 +1,17 @@
+//Variables//
+//This grabs the id of the first modal and sets it equal to modal
+var modal = document.getElementById("modal");
+//This grabs the button with the id "change"
+//This allows us to call a function with a click later
+var changeBtn = document.getElementById("change");
+//This grabs the preview
+var preview = document.getElementById("preview");
+
+//Functions//
+
+//Function to set Preview Boxes//
+
+//This sets the Preview Box color in the modal to the currently selected color
+function Preview() {
+    //This gets the currently selected color's value and sets it equal to color
+}
