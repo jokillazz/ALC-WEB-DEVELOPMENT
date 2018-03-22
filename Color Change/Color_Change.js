9f6f158963bf4e6c97d3a718dca93d7426a1dbc7
Unknown <joseph.c070@stu.nebo.edu>
2018-03-22 10:50:36 -0600

    Complete Color Change

--- Color Change/Color_Change.js
+++ Color Change/Color_Change.js
@@ -14,4 +14,53 @@ var preview = document.getElementById("preview");
 //This sets the Preview Box color in the modal to the currently selected color
 function Preview() {
     //This gets the currently selected color's value and sets it equal to color
+color = document.querySelector('input[name="colors"]:checked').value;
+//Log what color
+console.log("The color is " + color);
+//Change preview color
+preview.style.background = color;
+}
+
+//Function to change Color//
+
+//Change the Color
+function changeColor() {
+    //Log what the color was changed to 
+    console.log("The color is: " + color);
+    //Change the background color
+    document.body.style.background = color;
+}
+
+//Function to open modal//
+
+//Open color changing modal
+changeBtn.onclick = function() {
+    modal.style.visibility = "visible";
+    modal.style.opacity = "1";
+}
+
+// Function to close modal//
+
+//When the user clicks on the close button in modal it closes it
+function Close() {
+    //Close modal
+    modal.style.visibility = "hidden";
+    modal.style.opacity = "0";
+    // Reset Preview
+    prieview.style.background = "1a1a1a";
+    //Unselect any currently selected color
+    document.querySelector('input[name="colors"]:checked').checked = false;
+}
+
+//Close modal on window click
+window.onclick = function(event){
+    if (event.target == modal) {
+        //Close modal
+        modal.style.visibility = "hidden";
+        modal.style.opacity = "0";
+        // Reset Preview
+        preview.style.background = "#1a1a1a";
+        //Unselect any currently selected color
+        document.querySelector('input[name="colors"]:checked').checked = false;
+    }
 }
